2d1fa7642cb3c73d800eface0d72f7628aac86f9
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-11 11:43:30 +0100

    Added required pages
    The login, register, logout, settings and homepage.

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class UserController @Inject() extends Controller {
+
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+  def register = Action {
+    Ok(views.html.register())
+  }
+
+  def logout = Action {
+    Ok(Json.parse("""
+    {
+      "json": true
+    }
+    """))
+  }
+
+}
