4dd78aff7897707e87c10138d9afdae557cc8dad
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-19 16:12:33 +0100

    Fixed #18 Added custom favicon

--- app/models/Note.scala
+++ app/models/Note.scala
@@ -1,5 +1,8 @@
 package models
 
+/**
+ * This class is used to store notes. All notes are handled via this class.
+ */
 class Note(noteId: Long, noteTitle: String, noteContent: String, noteColor: String, noteOwners: Array[String]) {
   val id = noteId
   val title = noteTitle
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,5 +1,8 @@
 package models
 
+/**
+ * This class is used to store users. All users are handled via this class.
+ */
 class User(userEmail: String, userFirstName: String, userLastName: String, userPassword: String, userNotes: Array[String]) {
   val email = userEmail
   val firstName = userFirstName
