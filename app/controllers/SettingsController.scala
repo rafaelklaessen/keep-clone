c8934ace7a0f29ef6057df81189aa6df3a7b12bf
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-20 11:44:44 +0100

    Began working on settings frontend
    TODO: edit fields.

--- app/controllers/SettingsController.scala
+++ app/controllers/SettingsController.scala
@@ -4,6 +4,8 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import models.Users
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
@@ -14,7 +16,9 @@ class SettingsController @Inject() extends Controller {
   // Renders the settings page, but only when you're logged in
   def show = Action { request =>
     request.session.get("username").map { username => 
-      Ok(views.html.settings())
+      val user = Users.getUser(username)
+      
+      Ok(views.html.settings(username, user))
     }.getOrElse {
       NotFound("Page not found")
     }
