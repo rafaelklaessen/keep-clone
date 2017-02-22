26c04e7a8dcac368ced8a0190146452d6bf6813d
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-22 21:09:39 +0100

    Added Facebook OAuth

--- app/controllers/OAuthController.scala
+++ app/controllers/OAuthController.scala
@@ -80,7 +80,45 @@ class OAuthController @Inject() extends Controller {
       "oauth" -> "GitHub")
   }
 
-  def facebook = Action { request => 
-    Ok("facebook")
+  def facebook(code: String) = Action { request => 
+    // Get app ID and app secret    
+    val appId = "847737378702820"
+    val appSecret = "app_secret"
+
+    // Get access token
+    val accessTokenJsonUrl = "https://graph.facebook.com/v2.8/oauth/access_token?client_id=" + appId + "&redirect_uri=http%3A%2F%2Flocalhost:9000/oauth/facebook&client_secret=" + appSecret + "&code=" + code
+    val accessTokenJson = Json.parse(Source.fromURL(accessTokenJsonUrl).mkString)
+    val accessToken = (accessTokenJson \ "access_token").as[String]
+
+    // Verify that the access token actually is for our app
+    val debugTokenJsonUrl = "https://graph.facebook.com/debug_token?input_token=" + accessToken + "&access_token=" + appId + "|" + appSecret
+    val debugTokenJson = Json.parse(Source.fromURL(debugTokenJsonUrl).mkString)
+    val debugTokenData = (debugTokenJson \ "data")
+    
+    // If we're having a bad access token, reject. Otherwise,
+    // log the user in.
+    if ((debugTokenData \ "app_id").as[String] != appId) {
+      BadRequest("Invalid access token")
+    } else {
+      // Get user email
+      val userEmailJsonUrl = "https://graph.facebook.com/me?fields=email&access_token=" + accessToken 
+      val userEmailJson = Json.parse(Source.fromURL(userEmailJsonUrl).mkString)
+
+      // The ID functions as the username
+      val username = (debugTokenData \ "user_id").as[String]
+
+      val ref = FirebaseDatabase.getInstance().getReference("keep-clone")
+      val usersRef = ref.child("users")
+      val currentUser = usersRef.child(username)
+
+      // Create user in Firebase with its ID set to true.
+      // This way, the user exists verification still works.
+      currentUser.child(username).setValue(true)
+
+      Redirect("/").withSession(
+        "username" -> username,
+        "email" -> (userEmailJson \ "email").as[String],
+        "oauth" -> "Facebook")
+    }
   }
 }
