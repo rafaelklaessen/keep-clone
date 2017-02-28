827c4fdff73eef15a300cc370f77edeac76af395
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-02-28 21:11:45 +0100

    Finished note sharing popup frontend
    TODO: connect it to the backend.

--- public/es6/notesharing.js
+++ public/es6/notesharing.js
@@ -26,8 +26,8 @@ class NoteSharing {
     for (let i = 0; i < owners.length; i++) {
       ownerList += `
         <li class="note-owner">
-          <h4 class="note-owner-username">@${owners[i]}</h4>
-          <button class="material-icons delete-owner-btn md-btn btn">remove_circle</button>
+          <h4 class="note-owner-username">@<span class="username">${owners[i]}</span></h4>
+          <button class="material-icons remove-owner-btn md-btn btn">remove_circle</button>
         </li>
       `;
     }
@@ -35,7 +35,7 @@ class NoteSharing {
     // Generate popup HTML
     const $noteSharing = $(`
       <section class="overlay-black">
-        <section id="note-sharing" class="note-sharing popup" data-note="${id}" data-owners='${owners}'>
+        <section id="note-sharing" class="note-sharing popup" data-note="${id}" data-owners='${JSON.stringify(owners)}'>
           <header class="popup-title-container">
             <h2 class="popup-title">Sharing</h2>
           </header>
@@ -65,8 +65,122 @@ class NoteSharing {
     $noteSharing.find('.popup').fadeIn(400);
 
     // Add listeners
-    $noteSharign.find('.done-btn').click(function() {
+    $noteSharing.find('.username-input').on('keyup keydown click input propertychange', function() {
+      // Add valid class if there's a username given (in order to show the 
+      // check icon).
+      if ($(this).val().length > 0) {
+        $(this).addClass('valid');
+      } else {
+        $(this).removeClass('valid');
+      }
+    });
+
+    $noteSharing.find('.save-owner-btn').click(function() {
+      NoteSharing.addOwner();
+    });
+
+    $noteSharing.find('.remove-owner-btn').click(function() {
+      NoteSharing.removeOwner($(this).parent());
+    });
+
+    $noteSharing.find('.done-btn').click(function() {
       NoteSharing.done();
     });
+  }
+
+  /**
+   * NoteSharing.addOwner()
+   * Adds owner to note & updates DOM.
+   */
+  static addOwner() {
+    const $noteSharing = $('#note-sharing');
+    const $usernameInput = $noteSharing.find('.username-input');
+    const username = $usernameInput.val();
+
+    // Make sure the given username is valid
+    if ($usernameInput.hasClass('valid') && username.length > 0) {
+      // If user is already added, just clear the input
+      if ($noteSharing.data('owners').indexOf(username) >= 0) {
+        $usernameInput.val('');
+        $usernameInput.removeClass('valid');
+      } else {
+        // Get note ID
+        const id = $noteSharing.data('note');
+
+        // Update DOM
+        $(`
+            <li class="note-owner">
+              <h4 class="note-owner-username">@<span class="username">${username}</span></h4>
+              <button class="material-icons delete-owner-btn md-btn btn" onclick="NoteSharing.removeOwner($(this).parent());">remove_circle</button>
+            </li>
+          `)
+          .insertBefore($noteSharing.find('.add-note-owner'));
+        
+        // Add new owner to note DOM
+        const $note = $(`#${id}`);
+        $note.data('owners').push(username);
+        
+        // Add new owner to the sharing popup DOM
+        $noteSharing.data('owners').push(username);
+        
+        // Clear input
+        $usernameInput.val('');
+        $usernameInput.removeClass('valid');
+
+        // Perform request to backend
+        console.info('Post waddup');
+      }
+    }
+  }
+
+  /**
+   * NoteSharing.removeOwner()
+   * Removes owner from note & updates DOM.
+   * @param {object} noteOwnerEl The note-owner element.
+   */
+  static removeOwner(noteOwnerEl) {
+    // Transform note owner element to a jQuery object
+    const $noteOwner = $(noteOwnerEl);
+
+    const $noteSharing = $('#note-sharing');
+    // Get note ID
+    const id = $noteSharing.data('note');
+    // Get username of the owner to remove
+    const username = $noteOwner.find('.username').text().trim();
+
+    // Remove note owner element
+    $noteOwner.slideUp(200);
+    setTimeout(() => {
+      $noteOwner.remove();
+    }, 200);
+
+    // Remove owner from note & sharing popup DOM
+    const $note = $(`#${id}`);
+    const noteUsernameIndex = $note.data('owners').indexOf(username);
+    const sharingUsernameIndex = $noteSharing.data('owners').indexOf(username);
+
+    if (noteUsernameIndex >= 0 && sharingUsernameIndex >= 0) {
+      $note.data('owners').splice(noteUsernameIndex, 1)
+      $noteSharing.data('owners').splice(sharingUsernameIndex, 1);
+    } else {
+      console.warn('Couldn\'t remove owner from DOM.');
+    }
+
+    console.info(`Removing ${username} from ${id}`);
+  }
+
+  /**
+   * NoteSharing.done()
+   * Closes the sharing popup.
+   */
+  static done() {
+    $('#note-sharing')
+      .fadeOut(200)
+      .parent()
+      .fadeOut(400);
+
+    setTimeout(() => {
+      $('#note-sharing').parent().remove();
+    }, 400);
   }
 }
