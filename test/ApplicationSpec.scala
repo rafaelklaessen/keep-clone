5516744ce917f8bfc6c5141c0e6ea132cad2d712
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-03-18 21:11:22 +0100

    Finished HomeController tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -19,12 +19,34 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
   "HomeController" should {
 
-    "render the index page" in {
+    "render the index page when there is no user session" in {
       val home = route(app, FakeRequest(GET, "/")).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Welcome to Keep Clone")
+    }
+
+    "render the notes page when there is a user session" in {
+      val notes = route(app, FakeRequest(GET, "/").withSession(("username", "hold_on"))).get
+
+      status(notes) mustBe OK
+      contentType(notes) mustBe Some("text/html")
+      contentAsString(notes) must include ("<section id=\"write-note\" class=\"write-note\" data-color=\"#FFFFFF\">")
+    }
+
+    "render a 404 page instead of the archive page when there is no user session" in {
+      val archive = route(app, FakeRequest(GET, "/archive")).get
+
+      status(archive) mustBe NOT_FOUND
+    }
+
+    "render the archive page when there is a user session" in {
+      val archive = route(app, FakeRequest(GET, "/archive").withSession(("username", "hold_on"))).get
+
+      status(archive) mustBe OK
+      contentType(archive) mustBe Some("text/html")
+      contentAsString(archive) must include ("Archive")
     }
 
   }
