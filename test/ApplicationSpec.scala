63848c6c17cb127ed60fa04041be01d384c999dd
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-03-18 21:00:37 +0100

    Began working on tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -24,17 +24,7 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Your new application is ready.")
-    }
-
-  }
-
-  "CountController" should {
-
-    "return an increasing count" in {
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
+      contentAsString(home) must include ("Welcome to Keep Clone")
     }
 
   }
